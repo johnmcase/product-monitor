94e7d7ee2c4d854cd7e6b6e0e538ba454b5e19f4
John Case <johnmcase@gmail.com>
2016-03-21 15:28:37 -0500

    Initial commit

---
+++ ProductMonitor/Domain/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Domain
+{
+	public class Product
+	{
+		public Guid Id { get; set; }
+		public String VendorCode { get; set; }
+		public String Name { get; set; }
+		public String Description { get; set; }
+		public String Price { get; set; }
+	}
+}
---
+++ ProductMonitor/Domain/Vendor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Domain
+{
+	public class Vendor
+	{
+		public String Code { get; set; }
+		public String Name { get; set; }
+		public String Description { get; set; }
+	}
+}
