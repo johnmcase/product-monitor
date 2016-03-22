0bbcb080ce34c24c1bbba913202a34b267c8b11c
John Case <johnmcase@gmail.com>
2016-03-22 12:01:45 -0500

    Setup form layout with datagrids

--- ProductMonitor/Forms/MainForm.Designer.cs
+++ ProductMonitor/Forms/MainForm.Designer.cs
@@ -28,36 +28,70 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.SuspendLayout();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.vendorGrid = new System.Windows.Forms.DataGridView();
+			this.updatesGrid = new System.Windows.Forms.DataGridView();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.vendorGrid)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.updatesGrid)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// splitContainer1
+			// tableLayoutPanel1
 			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			this.splitContainer1.Size = new System.Drawing.Size(1173, 701);
-			this.splitContainer1.SplitterDistance = 319;
-			this.splitContainer1.TabIndex = 0;
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+			this.tableLayoutPanel1.Controls.Add(this.vendorGrid, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.updatesGrid, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1173, 701);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// vendorGrid
+			// 
+			this.vendorGrid.AllowUserToAddRows = false;
+			this.vendorGrid.AllowUserToDeleteRows = false;
+			this.vendorGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.vendorGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.vendorGrid.Location = new System.Drawing.Point(3, 3);
+			this.vendorGrid.Name = "vendorGrid";
+			this.vendorGrid.ReadOnly = true;
+			this.vendorGrid.Size = new System.Drawing.Size(345, 695);
+			this.vendorGrid.TabIndex = 0;
+			// 
+			// updatesGrid
+			// 
+			this.updatesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.updatesGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.updatesGrid.Location = new System.Drawing.Point(354, 3);
+			this.updatesGrid.Name = "dataGridView1";
+			this.updatesGrid.Size = new System.Drawing.Size(816, 695);
+			this.updatesGrid.TabIndex = 1;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1173, 701);
-			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "MainForm";
 			this.Text = "MainForm";
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.vendorGrid)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.updatesGrid)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.DataGridView vendorGrid;
+		private System.Windows.Forms.DataGridView updatesGrid;
+
 	}
 }
