c1af86fd5356baa7a39aecf9c25024c40a029c71
John Case <johnmcase@gmail.com>
2016-03-21 20:54:38 -0500

    Added VendorRepository integration test

---
+++ ProductMonitor.Test/Repository/VendorRepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductMonitor.Repository
+{
+	[TestClass]
+	public class VendorRepositoryTest
+	{
+		[TestMethod]
+		[TestCategory("Integration")]
+		public async Task GetVendors_WhenCalled_ReturnsData()
+		{
+			var repository = new VendorRepository();
+
+			var result = await repository.GetVendorsAsync();
+
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Count() > 0);
+		}
+	}
+}
